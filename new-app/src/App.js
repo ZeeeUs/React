26799bed247e884d2786625bea81dc55ad085360
Pogosyan Sos <pogosyan777.sp@gmail.com>
2021-02-10 00:53:50 +0300

    First React App

---
+++ new-app/src/App.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
+import Loader from './Loader'
+import Modal from "./Modal/Modal";
+
+function App() {
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then(todos => {
+        setTimeout(() => {
+        setTodos(todos)
+        setLoading(false)
+        }, 2000)
+      })
+  }, []);
+
+  function toggleTodo(id) {
+    console.log("todo id", id);
+  }
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className="App">
+        <h1>Hello from React</h1>
+        <AddTodo onCreate={addTodo} />
+        <Modal  />
+
+        {loading && <Loader />}
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : loading ? null : (
+          <p>No todos!</p>
+        )}
+      </div>
+    </Context.Provider>
+  );
+}
+
+export default App;
